4d37f3f818ce640668c06dd9488cb7a59ef69541
ymj0427 <zl0427ymj>
2020-05-13 17:16:17 +0800

    创建1.0分支

--- test/src/main/java/com/usian/java/Test2.java
+++ test/src/main/java/com/usian/java/Test2.java
@@ -2,7 +2,7 @@ package com.usian.java;
 
 public class Test2 {
     /**
-     * 1.0版本，正式进行开发，后续版本，请等待
+     * 1.0版本，正式进行开发，后续版本，请等待....
      *
      */
     public static void main(String[] args) {
