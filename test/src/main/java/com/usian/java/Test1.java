381d7d5ff2598ab82be742bcad0be388b6fdd572
ymj0427 <65162110+ymj0427@users.noreply.github.com>
2020-05-13 17:03:26 +0800

    Update Test1.java

--- test/src/main/java/com/usian/java/Test1.java
+++ test/src/main/java/com/usian/java/Test1.java
@@ -3,5 +3,7 @@ package com.usian.java;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("第一次写，要提交到本地库，然后push到github");
+        //有同事更新了代码，并上传到github=======
+        System.out.println("同事已更新代码，请你自己自行更新，以保同步");
     }
 }
