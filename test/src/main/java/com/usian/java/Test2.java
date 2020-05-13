0ef776182392a98bb3732e21956cc37ad328b7ad
ymj0427 <zl0427ymj>
2020-05-13 17:23:24 +0800

    主分支出了BUG，但继续发布

--- test/src/main/java/com/usian/java/Test2.java
+++ test/src/main/java/com/usian/java/Test2.java
@@ -8,4 +8,11 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("1.0版本");
     }
+
+    /**
+     *
+     * 本主分支。。。。出BUG啦啦啦啦啦！！！！不能影响用户使用，继续发布，提供使用
+     * 请由1.0分支帮我在幕后修改BUG
+     *
+     */
 }
